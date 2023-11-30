2889945aee4ac255a1c36fd76ee01a297033a37b
Joao Campos Costa <joao_cc@yahoo.com>
2023-11-30 14:40:54 +0100

    render user information from github

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 4,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 59.85438687728892, 
+            lng: 17.635608647952413
         }
     });
 
